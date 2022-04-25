f7e50e49cd3c306d1977b1210baf51fd888de75c
lhw421 <gpdnjs421@gmail.com>
2022-04-25 16:51:43 +0900

    css 오탈자 수정

--- js/10000hours.js
+++ js/10000hours.js
@@ -6,6 +6,9 @@ var goalValue = document.getElementById("goalValue");
 var timeValue = document.getElementById("timeValue");
 
 getResultButton.onclick = function(){
+    if(!goal.value || !time.value){
+        return;
+    }
     goalValue.innerHTML = goal.value;
     timeValue.innerHTML = Math.floor(10000 / parseInt(time.value));
     result.classList.remove("hide");
